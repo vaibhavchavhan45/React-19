901e257ff294b0a0392385930ca5343682f7a822
Vaibhav Chavhan <chavhanvaibhav708@gmail.com>
2025-09-09 06:05:05 +0000

    Use of Multiple States

--- 01_Basics/04_multipleStates/App.jsx
+++ 01_Basics/04_multipleStates/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0)
-  const [revCount, setrevCount] = useState(100)
+  const [revCount, setRevCount] = useState(100)
   return (
     <>
      <button onClick={() => {setCount(count + 1)}}>Forward Counter : {count}</button>
       <br /><br />
-     <button onClick={() => {setrevCount(revCount - 1)}}>Reverse Counter : {revCount}</button>
+     <button onClick={() => {setRevCount(revCount - 1)}}>Reverse Counter : {revCount}</button>
     </>
   );
 }
